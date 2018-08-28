d7d930db2191814a4fc7558f283ce7f1648af78f
Damir <pt7mk@yandex.ru>
2018-08-28 22:13:59 +0500

    Добаление тестового класса ContactCreationTests и вспомогательного класса ContactData

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string name;
+        private string middleName = "";
+
+        public ContactData(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string MiddleName
+        {
+            get
+            {
+                return middleName;
+            }
+            set
+            {
+                middleName = value;
+            }
+        }
+    }
+
+}
