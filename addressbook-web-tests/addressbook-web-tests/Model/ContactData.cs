62314dc0ed5001afb391cb1fd7e2647f18c943f7
Damir <pt7mk@yandex.ru>
2018-10-03 13:30:49 +0500

    Добавлены тесты для проверки информации на главной странице

--- addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
@@ -8,41 +8,84 @@ namespace WebAddressbookTests.Model
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
-        private string name;
-        private string lastName = "";
+        private string allEmails;
+        private string allPhones;
 
         public ContactData(string name)
         {
-            this.name = name;
+            Name = name;
         }
         public ContactData(string name, string lastName)
         {
-            this.name = name;
-            this.lastName = lastName;
+            Name = name;
+            LastName = lastName;
+        }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
 
-        }
-        public string Name
+        public string AllEmails
         {
             get
             {
-                return name;
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (CleanUpEmail(Email) + CleanUpEmail(Email2) + CleanUpEmail(Email3)).Trim();
+                }
             }
             set
             {
-                name = value;
+                allEmails = value;
             }
         }
-        public string LastName
+
+
+        public string AllPhones
         {
             get
             {
-                return lastName;
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUpPhone(HomePhone) + CleanUpPhone(MobilePhone) + CleanUpPhone(WorkPhone)).Trim();
+                }
             }
             set
             {
-                lastName = value;
+                allPhones = value;
             }
         }
+        private string CleanUpEmail(string email)
+        {
+            if (email == null || email == "")
+            {
+                return "";
+            }
+            return email + "\r\n";
+        }
+
+        private string CleanUpPhone(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+            }
+            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
+
         public bool Equals(ContactData other)
         {
             if (Object.ReferenceEquals(other, null))
